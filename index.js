3effc00df4467390179a2387506dd33bcb1093db
Neall Seth <32910906+neallseth@users.noreply.github.com>
2022-07-05 22:51:07 -0400

    PORT or 3000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cheerio = require('cheerio')
 const axios = require('axios')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', async (req, res) => {
     let data = await getSpaceNews();
